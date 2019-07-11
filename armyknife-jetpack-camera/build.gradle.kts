76227420435a3df49c286b8d10451b3e0da03064
eaglesakura <eagle.sakura@gmail.com>
2019-07-11 10:48:57 +0900

    chg: updated cameraX version.

--- armyknife-jetpack-camera/build.gradle.kts
+++ armyknife-jetpack-camera/build.gradle.kts
@@ -3,7 +3,7 @@ apply(from = "../dsl/ktlint.gradle")
 apply(from = "../dsl/bintray.gradle")
 
 dependencies {
-    "api"("com.eaglesakura.armyknife.armyknife-runtime:armyknife-runtime:1.3.1")
+    "api"("com.eaglesakura.armyknife.armyknife-runtime:armyknife-runtime:1.3.3")
     "compileOnly"("com.google.android.gms:play-services-vision:18.0.0")
     "api"("com.google.firebase:firebase-ml-vision:21.0.0")
 
@@ -11,6 +11,6 @@ dependencies {
     "api"("androidx.core:core-ktx:1.0.2")
 
     // TODO: check for updates(current version is alpha).
-    "api"("androidx.camera:camera-core:1.0.0-alpha02")
-    "api"("androidx.camera:camera-camera2:1.0.0-alpha02")
+    "api"("androidx.camera:camera-core:1.0.0-alpha03")
+    "api"("androidx.camera:camera-camera2:1.0.0-alpha03")
 }
